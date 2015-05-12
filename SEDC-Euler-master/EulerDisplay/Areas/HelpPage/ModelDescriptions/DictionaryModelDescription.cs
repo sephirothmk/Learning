a7db38b4f9ba6995d2a57943abc11ee7c4bc33f9
Vladimir Totocevski <sephirothmk@gmail.com>
2015-05-12 06:43:14 +0200

    meh

--- SEDC-Euler-master/EulerDisplay/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace EulerDisplay.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- SEDC-Euler-master/EulerDisplay/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace EulerDisplay.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
